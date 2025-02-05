179d2b433a02e8e5bb6b8e3dd4537b9b609f7f7e
Areeba Shakeel <shakeelareeba02@gmail.com>
2025-02-05 18:13:11 +0530

    ..

--- Dashboard/home.py
+++ Dashboard/home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Welcome to my Dashboard")
+st.markdown(' <h1 style="color: white; text-align:center; border: 2px dashed white; background-color:maroon; font-family:sans serif; margin-bottom:20px">  Titanic Dashboard  </h1>', unsafe_allow_html=True)
 
 st.image("Assets\img.jpg")
--- Dashboard/pages/dashboard.py
+++ Dashboard/pages/dashboard.py
@@ -11,5 +11,43 @@ titanic = sns.load_dataset("titanic")
 st.title("Titanic Dataset Dashboard")
 st.sidebar.header("Filter options")
 
-
+#to display the dataset
 st.dataframe(titanic)
+
+#Filters
+
+#Gender Filter
+gender = st.sidebar.multiselect('Gender',
+                                options = titanic['sex'].unique(),
+                                default = titanic['sex'].unique())
+                                
+
+#Class Filter
+pclass = st.sidebar.multiselect('Class',
+                                options = sorted(titanic['pclass'].unique()),   
+                                default = sorted(titanic['pclass'].unique()))
+
+#Age Filter
+min_age, max_age = st.sidebar.slider('Age',
+                                    min_value = int(titanic['age'].min()),
+                                    max_value = int(titanic['age'].max()),
+                                    value = (int(titanic['age'].min()), int(titanic['age'].max())))
+
+#Filter tha dataset based on selections, to check connectivity
+filtered_data = titanic[
+    (titanic['sex'].isin(gender)) &
+    (titanic['pclass'].isin(pclass)) &
+    (titanic['age'] >= min_age) &
+    (titanic['age'] <= max_age)
+]
+
+st.subheader("Gender Distribution")
+gender_count = filtered_data['sex'].value_counts()
+fig = px.pie(names=gender_count.index, values=gender_count.values, title = "Gender Distribution")
+st.plotly_chart(fig)
+
+#Create a histogram of age distribution
+st.subheader("Age Distribution")
+fig = px.histogram(filtered_data, x='age', nbins=20, title = "Age Distribution", 
+        labels = {'age': 'Age', 'count': 'Number of Passengers'})
+st.plotly_chart(fig)
